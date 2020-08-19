284d78459c020f60458cae7318d8263c9e9e6fe0
Sahil Deshmukh <sahil9677@gmail.com>
2020-08-18 22:34:44 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-sahil"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://9kn1ihys6c.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_3QZefBn4I",
+    APP_CLIENT_ID: "72lo7h54l6n6lr8u6ecd3rapma",
+    IDENTITY_POOL_ID: "us-east-1:5a4986b5-e800-4b4e-a30b-5dd8838085c7"
+  }
+};
