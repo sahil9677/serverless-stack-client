a7b44c59f45c5d8173a9cf4df67a4e48a4913ac4
Sahil Deshmukh <sahil9677@gmail.com>
2020-08-19 17:32:01 -0700

    Building our React app

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
   return (
@@ -10,9 +17,25 @@ export default function Routes() {
         <Home />
       </Route>
       {/* Finally, catch all unmatched routes */}
+      <UnauthenticatedRoute exact path="/login">
+  <Login />
+</UnauthenticatedRoute>
+<UnauthenticatedRoute exact path="/signup">
+  <Signup />
+</UnauthenticatedRoute>
+<AuthenticatedRoute exact path="/settings">
+  <Settings />
+</AuthenticatedRoute>
+<AuthenticatedRoute exact path="/notes/new">
+  <NewNote />
+</AuthenticatedRoute>
+<AuthenticatedRoute exact path="/notes/:id">
+  <Notes />
+</AuthenticatedRoute>
       <Route>
         <NotFound />
       </Route>
+
     </Switch>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
+
+  STRIPE_KEY: "pk_test_51HHIv5KZikXEzDO0mSh3PnHVckXIBXMLshaZpT6spgeZUl7Fktfsy84oP64KUBg7yg4xPEuj5zCmf6lO54588XHU005HDcgPAy",
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-uploads-sahil"
